eea9b3a759bb21111efe7255cd29e23ee6288652
Eric Sun <eric.sun@rpsrealsolutions.com>
2020-10-12 13:50:59 -0400

    ✨ add cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,39 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const KEEP_EVENT = false;
+
+admin.initializeApp();
+
+exports.updateTodo = functions.firestore.document('events/{Id}')
+  .onWrite((change, context) => {
+    const dat = change.after.data() as any;
+    if (!dat) return;
+
+    const { uid, event, data } = dat;
+    console.log('===>', uid, event, data);
+
+    const db = admin.firestore();
+    const del = () =>  !KEEP_EVENT && db.doc(`events/${context.params.Id}`).delete();
+
+    const todos = db.collection('/users/' + uid + '/todos');
+    switch (event) {
+      case '@create-todo':
+        data.timestamp = admin.firestore.FieldValue.serverTimestamp();
+        return todos.add(data).then(()=>del());
+      case '@update-todo':
+        return todos.doc(data.id).update(data).then(()=>del());
+      case '@delete-todo':
+        return todos.doc(data.id).delete().then(()=>del());
+      case '@delete-all-todo':
+        return todos.get().then(function (querySnapshot) {
+          const batch = db.batch();
+          querySnapshot.forEach(function (doc) {
+            batch.delete(doc.ref);
+          });
+          return batch.commit().then(()=>del());;
+        });
+      default: return;
+    }
+
 });
